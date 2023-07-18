b0e39695b697cd957a7cf4908da193e575bec7e1
anis520 <anisulhoque587@gmail.com>
2023-07-18 18:17:54 +0600

    done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,38 +1,50 @@
 import { useState } from "react";
+import "./Todo.css";
 
 const Todo = () => {
   const [input, setInput] = useState("");
   const [todoList, setTodoList] = useState([]);
-
   const handleOnchange = (even) => {
     setInput(even.target.value);
   };
 
   const handleDeleteTask = (i) => {
-    let newList = todoList.filter((item) => item !== i);
-    console.log(newList);
+    let newList = todoList.filter((x) => x !== i);
+    setTodoList(newList);
   };
 
   const handleOnSubmit = () => {
     setTodoList([...todoList, input]);
+    setInput("");
   };
 
   return (
     <>
-      <input
-        onChange={(even) => handleOnchange(even)}
-        type="text"
-        placeholder="Enter your task"
-        value={input}
-      />
-      <button onClick={handleOnSubmit}>Add To List</button>
-      {todoList.map((todo, i) => {
-        return (
-          <p key={i}>
-            {todo} <button onClick={() => handleDeleteTask(i)}>delet</button>
-          </p>
-        );
-      })}
+    <h1>React Todo App</h1>
+      <div className="maindiv">
+        <div className="maindiv1">
+          {todoList.map((todo, i) => {
+            return (
+              <p key={i}>
+                {todo}{" "}
+                <button onClick={() => handleDeleteTask(todo)}>delet</button>
+              </p>
+            );
+          })}
+
+          {todoList[0] == undefined && <p>no item found</p>}
+        </div>
+
+        <div className="maindiv2">
+          <input
+            onChange={(even) => handleOnchange(even)}
+            type="text"
+            placeholder="Enter your task"
+            value={input}
+          />
+          <button onClick={handleOnSubmit}>Add To List</button>
+        </div>
+      </div>
     </>
   );
 };
